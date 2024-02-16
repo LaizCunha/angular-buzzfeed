5531a512fe7018c4e278cd69e448cca43e2e8956
Laiz <laizsc.89@gmail.com>
2024-02-16 10:20:02 -0300

    cria as regras do projeto

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import quizz_questions from '../../../assets/data/quizz_questions.json';
 
 @Component({
   selector: 'app-quizz',
@@ -15,7 +16,7 @@ export class QuizzComponent implements OnInit {
   answers:string[] = []
   answerSelected:string = "";
 
-  questionIndez:number = 0;
+  questionIndex:number = 0;
   questionMaxIndex:number = 0;
 
   finished:boolean = false;
@@ -23,6 +24,31 @@ export class QuizzComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    if(quizz_questions){
+      this.finished = false;
+      this.title = quizz_questions.title;
+
+      this.questions = quizz_questions.questions;
+      this.questionSelected = this.questions[this.questionIndex]
+
+      this.questionIndex = 0;
+      this.questionMaxIndex = this.questions.length;
+    }
+  }
+
+  playerChoose(value:string) {
+    this.answers.push(value);
+    console.log(this.answers)
+  }
+
+  async nextSpet() {
+    this.questionIndex+=1;
+    if(this.questionMaxIndex > this.questionIndex) {
+      this.questionSelected = this.questions[this.questionIndex]
+    } else {
+      this.finished = true;
+    }
+
   }
 
 }
